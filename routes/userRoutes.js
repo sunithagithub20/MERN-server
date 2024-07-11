200efbd9911713b681b4e0f02fae935ce6e9e715
sunithagithub20 <lakshmisunitha20@gmail.com>
2024-07-11 13:52:09 +0600

    updates

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,56 +1,45 @@
-const express = require('express');
-const router = express.Router();
-const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express')
+const router = express.Router()
 const typeDefs = require('../schema');
 const resolvers = require('../resolvers');
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-router.post('/register', async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    const { data, errors } = await server.executeOperation({
-      query: gql`
-        mutation CreateUser($input: CreateUserInput!) {
-          createUser(input: $input) {
-            _id
-            name
-            email
-            password
-          }
-        }
-      `,
-      variables: {
-        input: { name, email, password }
-      }
-    });
-
-    if (errors) {
-      return res.status(500).send({ message: errors });
+const { ApolloServer,gql } = require('apollo-server-express');
+//:3001/users/register
+const server = new ApolloServer({typeDefs,resolvers});
+router.post('/register',async (req,res)=>{
+    try{
+        const {name,email,password} = req.body;
+        const {data,error}= await server.executeOperation({
+            query:gql`mutation{
+                createUser(input:{name:"${name}",email:"${email}",password:"${password}"}){
+                  _id
+                  email 
+                  name
+                  password
+                }
+              }`
+        });
+        if(error){return res.status(500).send({message:error}) }
+        res.status(201).send({message:"User Created",data:data});
+    }catch(err){
+        res.status(500).send({message:err});
     }
-
-    res.status(201).send(data);
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
-});
-router.get('/',async (req,res)=>{
-  try{
-     const {data,error}=await server.executeOperation({
-      query:gql`
-      query{
-      getAllUsers{
-      _id
-      name
-      email
-      password
-     }}`
-     })
-     if(error){return res.status(500).send({message:error})}
-     res.status(200).send(data)
-  }catch(err){
-    res.status(500).send({messange:err})
-  }
 })
+router.get('/',async (req,res)=>{
+    try{
+        const {data,error}= await server.executeOperation({
+            query:gql`
+                query{
+                    getAllUsers{
+                        _id 
+                        name
+                        email
+                        password
+                    }
+                }`
+        });
+        if(error){return res.status(500).send({message:error})}
+        res.status(200).send(data);
+    }catch(err){ res.status(500).send({message:err});}
+})
+
 module.exports = router;
